a9f7d670cc814f4eda92581035f57c6c94b5321b
Pascal Lombard <pascal.lombard@gmail.com>
2017-07-02 23:15:47 +0200

    - Mapper : Les "extension functions" de Kotlin permettent de remplacer avantageusement les mappers. A noter qu'il est possible d'étendre ainsi des classes provenant de librairies java par exemple.

--- src/main/java/mq/lombard/bottomsup/business/BeerHandler.kt
+++ src/main/java/mq/lombard/bottomsup/business/BeerHandler.kt
@@ -1,11 +1,12 @@
 package mq.lombard.bottomsup.business;
 
-import mq.lombard.bottomsup.Mapper
 import mq.lombard.bottomsup.Utils
 import mq.lombard.bottomsup.bean.BeerItem
 import mq.lombard.bottomsup.bean.Glass
 import mq.lombard.bottomsup.bean.GlassBuilder
 import mq.lombard.bottomsup.dao.BeerDao
+import mq.lombard.bottomsup.thirdparty.Beer
+import mq.lombard.bottomsup.toBeerItem
 import java.util.stream.Collectors
 
 /** @author Pascal Lombard */
@@ -22,7 +23,7 @@ object BeerHandler {
   }
 
   fun list(): Set<BeerItem> {
-    return BeerDao.listBeers().stream().map(Mapper::fromBeer)
+    return BeerDao.listBeers().stream().map(Beer::toBeerItem)
         .collect(Collectors.toSet());
   }
 }
