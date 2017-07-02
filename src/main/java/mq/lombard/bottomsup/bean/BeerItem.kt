4346675c96776b341ba7d879d9e9fa9ff9d1731b
Pascal Lombard <pascal.lombard@gmail.com>
2017-07-03 00:28:40 +0200

    - Data Class : Le POJO sauce Kotlin. Génère automatiquement getters, setters (si le champ est var-iable), hashcode(), equals() et toString(). Ajoute copy() en bonus.

---
+++ src/main/java/mq/lombard/bottomsup/bean/BeerItem.kt
@@ -0,0 +1,6 @@
+package mq.lombard.bottomsup.bean
+
+/** @author Pascal Lombard */
+data class BeerItem(var beerName: String,
+                    var description: String,
+                    var style: BeerStyle)
